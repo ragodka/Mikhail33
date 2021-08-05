50703796ef218d8198ef1628a51f2b4d5d40ba9c
ragodka <77529108+ragodka@users.noreply.github.com>
2021-08-05 13:22:16 +0300

    new site

---
+++ js/script1.js
@@ -0,0 +1,29 @@
+let offset = 0;
+function move() {
+  offset = offset + 5;
+  document.querySelector(".container").style.left = offset + "px";
+  if (offset > 350) {
+    return true;
+  }
+  // document.querySelector(".btn").onclick = move;
+  setTimeout(move, 30);
+}
+move();
+// document.querySelector(".btn").onclick = move;
+let offsetDrop = 0;
+function drop() {
+  offsetDrop = offsetDrop + 5;
+  document.querySelector(".box").style.top = offsetDrop + "px";
+  if (offsetDrop > 250) {
+    return true;
+  }
+  document.querySelector(".helicopter").onclick = drop;
+  setTimeout(drop, 10);
+}
+document.querySelector(".helicopter").onclick = drop;
+
+function showCat() {
+  document.querySelector(".box").style.right = "350px";
+  document.querySelector(".container").style.height = "723px";
+}
+document.querySelector(".box").onclick = showCat;
